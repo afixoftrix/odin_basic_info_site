49a265f593990f1be2f386723b740b63df6cb83c
Unwana Essien <unwana.essien@outlook.com>
2019-07-08 11:39:34 +0100

    basic project complete

--- src/index.js
+++ src/index.js
@@ -16,6 +16,58 @@ const server = http.createServer((req, res) => {
             res.end()
         })
     }
+    else if (req.url === '/about'){
+        const filepath = path.join( process.cwd(), "/public/about.html" )
+        console.log(filepath);
+        fs.readFile(filepath, (err, html) => {
+            if (err){ 
+                console.log(err);
+                return err
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.write(html)
+            res.end()
+        })
+    }
+    else if (req.url === '/contact-me'){
+        const filepath = path.join( process.cwd(), "/public/contact-me.html" )
+        console.log(filepath);
+        fs.readFile(filepath, (err, html) => {
+            if (err){ 
+                console.log(err);
+                return err
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html'})
+            res.write(html)
+            res.end()
+        })
+    }
+    else if ((req.url.indexOf(".css")) != -1){
+        const filepath = path.join( process.cwd(), "/public/styles.css" )
+        console.log(filepath);
+        fs.readFile(filepath, (err, css) => {
+            if (err){ 
+                console.log(err);
+                return err
+            }
+            res.writeHead(200, { 'Content-Type': 'text/css'})
+            res.write(css)
+            res.end()
+        })
+    }
+    else {
+        const filepath = path.join(process.cwd(), "/public/404.html");
+        console.log(filepath);
+        fs.readFile(filepath, (err, html) => {
+            if (err) {
+                console.log(err);
+                return err;
+            }
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.write(html);
+            res.end();
+        });
+    }
 })
 
 server.listen(3000, () => { console.log("listening on port 3000");})
